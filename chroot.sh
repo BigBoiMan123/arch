e3ce7fd7518e3589b90756971fd85ea65b68dbca
BigBoiMan123 <gandshuang@gmail.com>
2022-11-12 17:32:20 -0800

    fixes

--- chroot.sh
+++ chroot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "2997" | passwd --stdin root
+echo "root" | passwd --stdin root
 touch /etc/hostname
 sed '1i archspeedrun' /etc/hostname
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/timezone
@@ -7,6 +7,7 @@ locale-gen
 sed '1i en_US.UTF-8 UTF-8' /etc/locale.gen
 touch /etc/locale.conf
 sed '1i LANG=en_US.UTF-8' /etc/locale.conf
+sed 's/Required DatabaseOptional/Never' /etc/pacman.conf
 pacman -S grub efibootmgr --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
 grub-mkconfig -o /boot/grub/grub.cfg
