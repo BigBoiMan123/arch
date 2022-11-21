db8142b2a85b59495fcbf44cdea375760c4f77ff
BigBoiMan123 <96019531+BigBoiMan123@users.noreply.github.com>
2022-11-21 14:55:14 -0800

    fix setting password

--- chroot.sh
+++ chroot.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-touch passwd.txt
-sed -i '1i root:root' passwd.txt
-chpasswd <passwd.txt
+echo -e "root\nroot" | passwd root
 touch /etc/hostname
 sed -i '1i archspeedrun' /etc/hostname
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/timezone
@@ -11,6 +9,6 @@ touch /etc/locale.conf
 sed -i '1i LANG=en_US.UTF-8' /etc/locale.conf
 sed -i 's/Required DatabaseOptional/Never/' /etc/pacman.conf
 pacman -S grub efibootmgr --noconfirm
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=archspeedrun
 grub-mkconfig -o /boot/grub/grub.cfg
 exit
